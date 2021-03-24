a61c12df8c9b298c0c09a5c99cffde49e2feff75
Alves da Silva <tgsv@gft.com>
2021-03-23 21:29:09 -0300

    realizando commit

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,40 @@
-const input = document.querySelector('input[name="price"]')
-input.addEventListener("keydown", function(e){
+// const input = document.querySelector('input[name="price"]')
+// input.addEventListener("keydown", function(e){
     
 
-    setTimeout(function() {
-        let {value} = e.target
+//     setTimeout(function() {
+//         let {value} = e.target
 
-         value = value.replace(/\D/g,"")
+//          value = value.replace(/\D/g,"")
 
-         value = new Intl.NumberFormat('pt-br', {
-             style: 'currency',
-             currency: 'BRL'
-         }).format(value/100)
+//          value = new Intl.NumberFormat('pt-br', {
+//              style: 'currency',
+//              currency: 'BRL'
+//          }).format(value/100)
 
-         e.target.value = value
+//          e.target.value = value
 
-    }, 1)
+//     }, 1)
 
-})
+// })
+
+const Mask = {
+    apply(input, func){
+        setTimeout(function() {
+            input.value = Mask[func](input.value)
+        }, 1)
+
+    },
+    formatBRL(value){
+        value = value.replace(/\D/g,"")
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100)
+
+    }
+}
+
+
+    
 
